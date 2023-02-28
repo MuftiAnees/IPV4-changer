f9f787744033cbbccfb4f9cdac3a5f8a2836b34a
Anees Mufti <89216202+MuftiAnees@users.noreply.github.com>
2023-02-28 17:49:26 +0500

    Update OFF.bat
    Fixed not turning Off bug

--- OFF.bat
+++ OFF.bat
@@ -1,5 +1,5 @@
 netsh interface ipv4 set address name="Ethernet" source=static ^
-      addr=172.16.5.35 mask=255.255.255.0 gateway=172.16.5.254
+      addr=172.160.5.35 mask=255.255.255.0 gateway=172.16.5.254
 netsh interface ip add dns name="Ethernet" addr=
 netsh interface ip add dns name="Ethernet" addr= index=2
 rem job done, then exit with a pause before
